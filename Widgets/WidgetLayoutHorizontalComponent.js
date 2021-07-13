ce074f14350a54bcda28533312462721ac934303
Abraham Globex <forraidplay@gmail.com>
2021-07-13 11:03:53 +0700

    format wLayHrComp

--- Widgets/WidgetLayoutHorizontalComponent.js
+++ Widgets/WidgetLayoutHorizontalComponent.js
@@ -1,125 +1,118 @@
 class WidgetLayoutHorizontal extends WidgetLayout {
+  constructor() {
+    super();
+    this.ratioX = 0;
+    this.ratioY = 0;
 
-	constructor() {
-		super();
-		this.ratioX = 0;
-		this.ratioY = 0;
+    this.space = 0;
 
-		this.space = 0;
+    this.htmlElement.classList.add("WidgetLayoutHorizontal");
 
-		this.htmlElement.classList.add("WidgetLayoutHorizontal");
+    this._childrenAlignType = null;
+  }
 
-		this._childrenAlignType = null;
+  eventHandler(eventType, eventObject) {
+    eventObject.persist();
+    console.log(
+      "eventHandler",
+      eventType,
+      eventObject.target.value,
+      eventObject.target
+    );
+    Module.Store.dispatch({
+      eventName: this.props.widgets[this.id].events[eventType],
+      value: eventObject,
+    });
+  }
 
-	}
+  render() {
+    const widget = this.props.widgets[this.id];
 
-	eventHandler(eventType, eventObject) {
+    let eventAttributes = {};
 
-		eventObject.persist();
-		console.log('eventHandler', eventType, eventObject.target.value, eventObject.target);
-		Module.Store.dispatch({
-			'eventName': this.props.widgets[this.id].events[eventType],
-			'value': eventObject
-		});
-	}
+    for (let eventName in widget.events) {
+      eventAttributes[eventName] = (event) =>
+        this.eventHandler(eventName, event);
+    }
 
-	render() {
+    // return (
+    // 	// Base LayoutHorizontal Html
+    // 	<div
+    // 		id={this.id}
+    // 		className="WidgetLayoutHorizontal"
+    // 		{...widget.attributes}
+    // 		{...eventAttributes}
+    // 	>
+    // 	</div>
+    // );
+  }
 
-		const widget = this.props.widgets[this.id];
+  onComponentDidMount() {}
 
-		let eventAttributes = {};
-		
-		
-		for (let eventName in widget.events) {
-			eventAttributes[eventName] = (event) => this.eventHandler(eventName, event);
-		}
-		
-		// return (
-		// 	// Base LayoutHorizontal Html
-		// 	<div
-		// 		id={this.id}
-		// 		className="WidgetLayoutHorizontal"
-		// 		{...widget.attributes}
-		// 		{...eventAttributes}
-		// 	>
-		// 	</div>
-		// );
+  appendChild(child) {
+    super.appendChild(child);
 
-	}
-	
-	onComponentDidMount() {
+    this.controlChildrenHorizontalAlignType(this._childrenHorizontalAlignType);
+    this.controlChildrenVerticalAlignType(this._childrenVerticalAlignType);
+  }
 
+  set spacing(value) {
+    if (value == null) return;
+    if (value < 0) return;
 
-	}
-	
-	appendChild(child) {
-		super.appendChild(child);
+    this._spacing = value;
 
-		this.controlChildrenHorizontalAlignType(this._childrenHorizontalAlignType);
-		this.controlChildrenVerticalAlignType(this._childrenVerticalAlignType);
-	}
+    for (let i = 0; i < this.htmlElement.children.length - 1; ++i) {
+      const child = this.htmlElement.children[i];
+      if (child == null) continue;
+      child.style.marginRight = value + "px";
+    }
 
-	set spacing(value) {
-		if (value == null) return;
-		if (value < 0) return;
+    if (this.htmlElement.children.length > 1) {
+      let lastChild =
+        this.htmlElement.children[this.htmlElement.children.length - 1];
+      if (lastChild != null) lastChild.style.marginRight = null;
+    }
+  }
 
-		this._spacing = value;
+  controlChildrenHorizontalAlignType(alignmentType) {
+    if (!alignmentType) return;
 
-		for (let i = 0; i < this.htmlElement.children.length - 1; ++i) {
-			const child = this.htmlElement.children[i];
-			if (child == null) continue;
-			child.style.marginRight = value + "px";
-		}
+    this._childrenHorizontalAlignType = alignmentType;
 
-		if (this.htmlElement.children.length > 1) {
-			let lastChild = this.htmlElement.children[this.htmlElement.children.length - 1];
-			if (lastChild != null)
-				lastChild.style.marginRight = null;
-		}
+    if (this.hasScroll()) {
+      this.htmlElement.style.justifyContent = "";
+      return;
+    }
 
-	}
+    if (alignmentType === 0) this.htmlElement.style.justifyContent = "";
+    else if (alignmentType === 1)
+      this.htmlElement.style.justifyContent = "flex-start";
+    else if (alignmentType === 2)
+      this.htmlElement.style.justifyContent = "center";
+    else if (alignmentType === 3)
+      this.htmlElement.style.justifyContent = "flex-end";
+  }
 
-	controlChildrenHorizontalAlignType(alignmentType) {
-		if (!alignmentType)
-			return;
+  controlChildrenVerticalAlignType(alignmentType) {
+    if (!alignmentType) return;
 
-		this._childrenHorizontalAlignType = alignmentType;
+    if (alignmentType === 0)
+      //none = top
+      alignmentType = 1;
 
-		if (this.hasScroll()) {
-			this.htmlElement.style.justifyContent = "";
-			return;
-		}
+    this._childrenVerticalAlignType = alignmentType;
 
-		if (alignmentType === 0)
-			this.htmlElement.style.justifyContent = "";
-		else if (alignmentType === 1)
-			this.htmlElement.style.justifyContent = "flex-start";
-		else if (alignmentType === 2)
-			this.htmlElement.style.justifyContent = "center";
-		else if (alignmentType === 3)
-			this.htmlElement.style.justifyContent = "flex-end";
-	}
+    if (alignmentType === 1) this.htmlElement.style.alignItems = "flex-start";
+    else if (alignmentType === 2) this.htmlElement.style.alignItems = "center";
+    else if (alignmentType === 3)
+      this.htmlElement.style.alignItems = "flex-end";
+  }
 
-	controlChildrenVerticalAlignType(alignmentType) {
-		if (!alignmentType)
-			return;
-
-		if (alignmentType === 0) //none = top
-			alignmentType = 1;
-
-		this._childrenVerticalAlignType = alignmentType;
-
-		if (alignmentType === 1)
-			this.htmlElement.style.alignItems = "flex-start";
-		else if (alignmentType === 2)
-			this.htmlElement.style.alignItems = "center";
-		else if (alignmentType === 3)
-			this.htmlElement.style.alignItems = "flex-end";
-	}
-
-	hasScroll() {
-		return Math.abs(this.htmlElement.scrollWidth - this.htmlElement.clientWidth) !== 0;
-	}
-
-
+  hasScroll() {
+    return (
+      Math.abs(this.htmlElement.scrollWidth - this.htmlElement.clientWidth) !==
+      0
+    );
+  }
 }
