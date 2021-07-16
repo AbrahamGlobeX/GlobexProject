0318605351679229d3a52293c7cd527a18d04d08
Abraham Globex <forraidplay@gmail.com>
2021-07-16 12:43:26 +0700

    test1

--- PropertyEditor/types/characteristics/boolean.js
+++ PropertyEditor/types/characteristics/boolean.js
@@ -17,16 +17,4 @@ class Characteristics_BooleanType extends BasePropertyType{
         widget.includeWidget(checkbox);
         this.widget = widget;
     }
-
-    static async getInputWidget(property) {
-        const widget = new WidgetLayoutHorizontal();
-        const input = new WidgetCheckBox();
-        input.checked = property.value;
-        input.inputElement.onchange = (e) => {
-            property.value = e.target.checked;
-        }
-
-        widget.includeWidget(input);
-        return widget;
-    }
 }
